0d8dd5d0b0a8cb62929b1f2a70fcd60ed9127d1d
Alexey <aleks-samos@yandex.ru>
2021-10-15 22:57:39 +0300

    spaces!!!

--- parsevoices.py
+++ parsevoices.py
@@ -2,7 +2,7 @@ import html
 import json
 import re
 with open("voicesplus.txt") as f: table = f.read()
-table = table.replace("\r", "").replace("\n", "").replace("<code>", "").replace("</code>", "").replace("<sup>New</sup>", "").replace(" <sup>New</sup>", "")
+table = table.replace("\r", "").replace("\n", "").replace("<code>", "").replace("</code>", "").replace("<sup>New</sup>", "")
 
 json_voices_list = []
 tbody = re.findall(r"<tbody>(.*?)</tbody>", table, re.M+re.I)[0]
@@ -10,15 +10,15 @@ trs = re.findall(r"<tr>(.*?)</tr>", tbody, re.M+re.I)
 
 for tr in trs:
 	tds = re.findall(r"<td>(.*?)</td>", tr, re.M+re.I)
-	status = tds[4].split(",", 1)[0]
-	locale = tds[1]
-	name = tds[3].replace(locale+"-", "")
+	status = tds[4].split(",", 1)[0].strip()
+	locale = tds[1].strip()
+	name = tds[3].replace(locale+"-", "").strip()
 	json_voices_list.append({
 		"Name":f"Microsoft Server Speech Text to Speech Voice ({locale}, {name})",
-		"ShortName":f"{tds[3]}",
-		"Gender":f"{tds[2]}",
+		"ShortName":f"{tds[3].strip()}",
+		"Gender":f"{tds[2].strip()}",
 		"Locale":locale,
-		"FriendlyName":f"{name.replace('Neural', '')} - {tds[0]}",
+		"FriendlyName":f"{name.replace('Neural', '').strip()} - {tds[0].strip()}",
 		"SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
 		"Status":status
 	})
