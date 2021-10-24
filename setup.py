7512f9f453e9d30601af0c3771c4538c82eca48d
Alexey <aleks-samos@yandex.ru>
2021-10-24 12:06:07 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,45 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
+#!/bin/env python
+
+import os
 import sys
-if sys.version_info[0] != 3 or sys.version_info[1] < 6:
-	print("This script requires Python >= 3.6")
-	exit(1)
+from os.path import dirname
+from os.path import join as pathjoin
+from setuptools import find_packages
+from setuptools import setup
+
+
+NAME = 'msspeech'
+VERSION = '2.8'
+
+
+with open(pathjoin(dirname(__file__), 'README.md')) as f:
+    DESCRIPTION = f.read()
+
+REQUIRED, REQUIRED_URL = [], []
+
+with open('requirements.txt') as f:
+    for line in f.readlines():
+        if 'http://' in line or 'https://' in line:
+            REQUIRED_URL.append(line)
+
+        else:
+            REQUIRED.append(line)
+
+
+packages = sorted(set(find_packages()))
 
 setup(
-    name="msspeech",
-    version="2.7",
-    author="alekssamos",
-    author_email="aleks-samos@yandex.ru",
-    url="https://github.com/alekssamos/msspeech/",
-    packages=find_packages(),
+    name=NAME,
+    version=VERSION,
+    description='Microsoft speech synthesis from Microsoft Edge web browser read aloud.',
+    long_description=DESCRIPTION,
+    author='alekssamos',
+    author_email='aleks-samos@yandex.ru',
+    maintainer='alekssamos',
+    maintainer_email='aleks-samos@yandex.ru',
+    url='https://github.com/alekssamos/msspeech/',
+    packages=packages,
+    install_requires=REQUIRED,
+    dependency_links=REQUIRED_URL,
     include_package_data=True,
-    long_description_content_type="text/markdown",
-    long_description=open(join(dirname(__file__), "README.md"), encoding="UTF8").read(),
 )
