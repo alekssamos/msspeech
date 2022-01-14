634d7049af561e5cdd13b84999e2cd48443b33c9
Alexey <aleks-samos@yandex.ru>
2022-01-14 23:47:10 +0300

    change version

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-NAME = 'msspeech'
-VERSION = '2.10'
+NAME = "msspeech"
+VERSION = "2.13"
 
 
-with open(pathjoin(dirname(__file__), 'README.md')) as f:
+with open(pathjoin(dirname(__file__), "README.md")) as f:
     DESCRIPTION = f.read()
 
 REQUIRED, REQUIRED_URL = [], []
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     for line in f.readlines():
-        if 'http://' in line or 'https://' in line:
+        if "http://" in line or "https://" in line:
             REQUIRED_URL.append(line)
 
         else:
@@ -31,13 +31,13 @@ packages = sorted(set(find_packages()))
 setup(
     name=NAME,
     version=VERSION,
-    description='Microsoft speech synthesis from Microsoft Edge web browser read aloud.',
+    description="Microsoft speech synthesis from Microsoft Edge web browser read aloud.",
     long_description=DESCRIPTION,
-    author='alekssamos',
-    author_email='aleks-samos@yandex.ru',
-    maintainer='alekssamos',
-    maintainer_email='aleks-samos@yandex.ru',
-    url='https://github.com/alekssamos/msspeech/',
+    author="alekssamos",
+    author_email="aleks-samos@yandex.ru",
+    maintainer="alekssamos",
+    maintainer_email="aleks-samos@yandex.ru",
+    url="https://github.com/alekssamos/msspeech/",
     packages=packages,
     install_requires=REQUIRED,
     dependency_links=REQUIRED_URL,
