37a2a8a4aa5114359ae469c2ba5b03ffa2e1bd02
Alexey <aleks-samos@yandex.ru>
2022-10-12 19:13:39 +0300

    add parse multi voices test

---
+++ tests/parse_multi_voices_test.py
@@ -0,0 +1,9 @@
+import asyncio
+import pytest
+
+@pytest.mark.asyncio
+async def test_parse_multivoices_wwith_default_params(mss):
+    input_text = "%Guy: Hi, Aria!\n%Aria: Hello, Guy!".strip()
+    expected_ssml = "<voice  name='en-US-GuyNeural'><prosody pitch='0Hz' rate ='+0%' volume='+1.0'> Hi, Aria!</prosody></voice>\n\n<voice  name='en-US-AriaNeural'><prosody pitch='0Hz' rate ='+0%' volume='+1.0'> Hello, Guy!</prosody></voice>".strip()
+    result = (await mss.parse_multivoices(input_text)).strip()
+    assert expected_ssml == result
