d9ad866ac3d4578b5452d40b95ba5a214bf78782
Alexey <aleks-samos@yandex.ru>
2022-10-16 19:36:05 +0300

    websocket test

--- tests/api_test.py
+++ tests/api_test.py
@@ -7,3 +7,10 @@ async def test_get_voices_list_api(cli_srv_mss):
     client, server, mss = cli_srv_mss
     voices = await mss.get_voices_list()
     assert len(voices) == 3  # check API mock
+
+@pytest.mark.asyncio
+async def test_synthesize_api(cli_srv_mss):
+    client, server, mss = cli_srv_mss
+    vn = (await mss.get_voices_by_substring("Guy"))[0]["Name"]
+    await mss.set_voice(vn)
+    await mss.synthesize("hi!", "test.opus")
