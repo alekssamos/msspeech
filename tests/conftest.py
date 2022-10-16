24d26916a70f637964a6189bb79a23ae0fd66c25
Alexey <aleks-samos@yandex.ru>
2022-10-16 17:09:41 +0300

    try fix JSON errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -72,57 +72,53 @@ async def cli_srv_mss(
         return web.json_response(
             json.loads(
                 """[
-                  {
-                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
-                    "ShortName": "en-US-JennyNeural",
-                    "Gender": "Female",
-                    "Locale": "en-US",
-                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                    "FriendlyName": "Microsoft Jenny Online (Natural) - English (United States)",
-                    "Status": "GA",
-                    "VoiceTag": {
-                      "ContentCategories": [
-                        "Conversation",
-                        "News"
-                      ],
-                      "VoicePersonalities": [
-                        "Friendly",
-                        "Considerate",
-                        "Comfort"
-                      ]
-                    }
-                  },
-                {
-                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
-                    "ShortName": "en-US-AriaNeural",
-                    "Gender": "Female",
-                    "Locale": "en-US",
-                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                    "FriendlyName": "Microsoft Aria Online (Natural) - English (United States)",
-                    "Status": "GA",
-                    "VoiceTag": {
-                        "ContentCategories": ["News", "Novel"],
-                        "VoicePersonalities": ["Positive", "Confident"],
-                    },
-                  },
-                  {
-                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
-                    "ShortName": "en-US-GuyNeural",
-                    "Gender": "Male",
-                    "Locale": "en-US",
-                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                    "FriendlyName": "Microsoft Guy Online (Natural) - English (United States)",
-                    "Status": "GA",
-                    "VoiceTag": {
-                      "ContentCategories": [
-                        "News",
-                        "Novel"
-                      ],
-                      "VoicePersonalities": [
-                        "Passion"
-                      ]
-                    }
-                  },
+            {
+                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
+                "ShortName": "en-US-JennyNeural",
+                "Gender": "Female",
+                "Locale": "en-US",
+                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                "FriendlyName": "Microsoft Jenny Online (Natural) - English (United States)",
+                "Status": "GA",
+                "VoiceTag": {
+                  "ContentCategories": [
+                    "Conversation",
+                    "News"
+                  ],
+                  "VoicePersonalities": [
+                    "Friendly",
+                    "Considerate",
+                    "Comfort"
+                  ]
+                }
+            },
+            {
+                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
+                "ShortName": "en-US-GuyNeural",
+                "Gender": "Male",
+                "Locale": "en-US",
+                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                "FriendlyName": "Microsoft Guy Online (Natural) - English (United States)",
+                "Status": "GA",
+                "VoiceTag": {
+                    "ContentCategories": ["News", "Novel"],
+                    "VoicePersonalities": ["Passion"],
+                },
+            },
+            {
+                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
+                "ShortName": "en-US-AriaNeural",
+                "Gender": "Female",
+                "Locale": "en-US",
+                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                "FriendlyName": "Microsoft Aria Online (Natural) - English (United States)",
+                "Status": "GA",
+                "VoiceTag": {
+                    "ContentCategories": ["News", "Novel"],
+                    "VoicePersonalities": ["Positive", "Confident"],
+                },
+            },
+
                 ]"""
             )
         )
