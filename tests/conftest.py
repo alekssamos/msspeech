4a9c0b5199f6d90645b5eaef73b77df9fd27e587
Alexey <aleks-samos@yandex.ru>
2022-10-16 15:05:20 +0300

    rewrite servertest

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,18 +46,16 @@ def mss(monkeypatch):
 
 
 @pytest.fixture
-def cli_srv() -> Tuple[TestClient, TestServer]:
+def mss_cli_srv(monkeypatch) -> Tuple[TestClient, TestServer]:
     "Create and return mock client and server form msspeech API"
     from aiohttp import web
+    from unittest.mock import mock_open
     import json
 
     @web.middleware
     async def check_request(request, handler):
         "check token"
-        if (
-            request.query.get("trustedclienttoken", "")
-            != "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
-        ):
+        if request.query.get("trustedclienttoken", "") != "testtoken":
             return web.Response(body="", status=401)
         response = await handler(request)
         return response
@@ -67,136 +65,73 @@ def cli_srv() -> Tuple[TestClient, TestServer]:
 
     @routes.get("/consumer/speech/synthesize/readaloud/voices/list")
     async def get_voices_list_handler(request):
+        "Returns a list of 3 voices"
         return web.json_response(
             json.loads(
                 """[
-            {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
-                "ShortName": "en-US-AriaNeural",
-                "Gender": "Female",
-                "Locale": "en-US",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Aria Online (Natural) - English (United States)",
-                "Status": "GA",
-                "VoiceTag": {
-                    "ContentCategories": ["News", "Novel"],
-                    "VoicePersonalities": ["Positive", "Confident"],
-                },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
-                "ShortName": "en-US-GuyNeural",
-                "Gender": "Male",
-                "Locale": "en-US",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Guy Online (Natural) - English (United States)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "News",
-                    "Novel"
-                  ],
-                  "VoicePersonalities": [
-                    "Passion"
-                  ]
-                }
-              },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
-                "ShortName": "en-US-JennyNeural",
-                "Gender": "Female",
-                "Locale": "en-US",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Jenny Online (Natural) - English (United States)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "Conversation",
-                    "News"
-                  ],
-                  "VoicePersonalities": [
-                    "Friendly",
-                    "Considerate",
-                    "Comfort"
-                  ]
-                }
-              },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, MichelleNeural)",
-                "ShortName": "en-US-MichelleNeural",
-                "Gender": "Female",
-                "Locale": "en-US",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Michelle Online (Natural) - English (United States)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "News",
-                    "Novel"
-                  ],
-                  "VoicePersonalities": [
-                    "Friendly",
-                    "Pleasant"
-                  ]
-                }
-              },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-US, RogerNeural)",
-                "ShortName": "en-US-RogerNeural",
-                "Gender": "Male",
-                "Locale": "en-US",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Roger Online (Natural) - English (United States)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "News",
-                    "Novel"
-                  ],
-                  "VoicePersonalities": [
-                    "Lively"
-                  ]
-                }
-              },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-ZA, LeahNeural)",
-                "ShortName": "en-ZA-LeahNeural",
-                "Gender": "Female",
-                "Locale": "en-ZA",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Leah Online (Natural) - English (South Africa)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "General"
-                  ],
-                  "VoicePersonalities": [
-                    "Friendly",
-                    "Positive"
-                  ]
-                }
-              },
-              {
-                "Name": "Microsoft Server Speech Text to Speech Voice (en-ZA, LukeNeural)",
-                "ShortName": "en-ZA-LukeNeural",
-                "Gender": "Male",
-                "Locale": "en-ZA",
-                "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
-                "FriendlyName": "Microsoft Luke Online (Natural) - English (South Africa)",
-                "Status": "GA",
-                "VoiceTag": {
-                  "ContentCategories": [
-                    "General"
-                  ],
-                  "VoicePersonalities": [
-                    "Friendly",
-                    "Positive"
-                  ]
-                }
-              }
-        ]"""
+                  {
+                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
+                    "ShortName": "en-US-JennyNeural",
+                    "Gender": "Female",
+                    "Locale": "en-US",
+                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                    "FriendlyName": "Microsoft Jenny Online (Natural) - English (United States)",
+                    "Status": "GA",
+                    "VoiceTag": {
+                      "ContentCategories": [
+                        "Conversation",
+                        "News"
+                      ],
+                      "VoicePersonalities": [
+                        "Friendly",
+                        "Considerate",
+                        "Comfort"
+                      ]
+                    }
+                  },
+                {
+                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
+                    "ShortName": "en-US-AriaNeural",
+                    "Gender": "Female",
+                    "Locale": "en-US",
+                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                    "FriendlyName": "Microsoft Aria Online (Natural) - English (United States)",
+                    "Status": "GA",
+                    "VoiceTag": {
+                        "ContentCategories": ["News", "Novel"],
+                        "VoicePersonalities": ["Positive", "Confident"],
+                    },
+                  {
+                    "Name": "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
+                    "ShortName": "en-US-GuyNeural",
+                    "Gender": "Male",
+                    "Locale": "en-US",
+                    "SuggestedCodec": "audio-24khz-48kbitrate-mono-mp3",
+                    "FriendlyName": "Microsoft Guy Online (Natural) - English (United States)",
+                    "Status": "GA",
+                    "VoiceTag": {
+                      "ContentCategories": [
+                        "News",
+                        "Novel"
+                      ],
+                      "VoicePersonalities": [
+                        "Passion"
+                      ]
+                    }
+                  }
+                ]"""
             )
         )
 
     test_server = TestServer(app)
     test_client = TestClient(test_server)
-    return (test_client, test_server)
+    monkeypatch.setattr("msspeech.os.path.isfile", lambda x: False)
+    monkeypatch.setattr("msspeech.open", mock_open)
+    monkeypatch.setattr("msspeech._voices_list", [])
+    monkeypatch.setattr(
+        "msspeech.MSSpeech.endpoint",
+        f"{test_server.scheme}://{test_server.host}:{test_server.port}",
+    )
+    monkeypatch.setattr("msspeech.MSSpeech.trustedclienttoken", "testtoken")
+    yield (test_client, test_server)
+    test_client.close()
