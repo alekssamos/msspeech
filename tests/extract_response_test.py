9eeb909303397122588a8f8c891c76bea47be536
Alexey <aleks-samos@yandex.ru>
2022-10-16 18:47:12 +0300

    move audio response to conftest and 1 assert headers

--- tests/extract_response_test.py
+++ tests/extract_response_test.py
@@ -14,7 +14,7 @@ async def test_extract_response_str(mss):
 
 @pytest.mark.asyncio
 # @pytest.mark.xfail()
-async def test_extract_response_bytes(mss):
+async def test_extract_response_bytes(mss, sp_audio):
     expected_headers = {
         "X-RequestId": "586e68cb7617113bee75",
         "Content-Type": "audio/webm; codec=opus",
@@ -22,17 +22,7 @@ async def test_extract_response_bytes(mss):
         "Path": "audio",
     }
     expected_body = b"theaudiofile"
-    response = (
-        b".."
-        + b"X-RequestId:586e68cb7617113bee75\r\n"
-        + b"Content-Type:audio/webm; codec=opus\r\n"
-        + b"X-StreamId:D9C8CDE8B3E2451D84D416C9A44310CC\r\n"
-        + b"Path:audio\r\n"
-        + b"theaudiofile"
-    )
+    response = sp_audio
     headers, body = mss._extract_response(response)
     assert body == expected_body
-    # assert headers["X-RequestId"] == expected_headers["X-RequestId"]
-    assert headers["X-StreamId"] == expected_headers["X-StreamId"]
-    assert headers["Content-Type"] == expected_headers["Content-Type"]
-    assert headers["Path"] == expected_headers["Path"]
+    assert headers == expected_headers
